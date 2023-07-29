d21a57e9ca11c7b7c10073aa9a0da1fbdf68a55e
Wilson Silva <wilson.dsigns@gmail.com>
2023-07-29 11:55:50 +0700

    Implement register, sign in and profile pages (with logout)

--- lib/src/screens/custom_sign_in_screen.dart
+++ lib/src/screens/custom_sign_in_screen.dart
@@ -1,14 +1,30 @@
+import 'package:faker_app_flutter_firebase/src/routing/app_router.dart';
+import 'package:firebase_ui_auth/firebase_ui_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
 
 class CustomSignInScreen extends ConsumerWidget {
   const CustomSignInScreen({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final authProviders = [EmailAuthProvider()];
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Sign in'),
+      ),
+      body: SignInScreen(
+        providers: authProviders,
+        actions: [
+          AuthStateChangeAction<SignedIn>((context, state) {
+            context.goNamed(AppRoute.profile.name);
+          }),
+          AuthStateChangeAction<UserCreated>((context, state) {
+            context.goNamed(AppRoute.profile.name);
+          })
+        ],
       ),
     );
   }
