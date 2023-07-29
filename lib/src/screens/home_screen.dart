6cebc0b165150bf5f98e7ed8efb50444aaad0dfb
Wilson Silva <wilson.dsigns@gmail.com>
2023-07-29 13:22:10 +0700

    List jobs

--- lib/src/screens/home_screen.dart
+++ lib/src/screens/home_screen.dart
@@ -1,6 +1,9 @@
-import 'package:faker/faker.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:faker/faker.dart' hide Job;
 import 'package:faker_app_flutter_firebase/src/data/firestore_repository.dart';
+import 'package:faker_app_flutter_firebase/src/data/job.dart';
 import 'package:faker_app_flutter_firebase/src/routing/app_router.dart';
+import 'package:firebase_ui_firestore/firebase_ui_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
@@ -32,6 +35,29 @@ class HomeScreen extends ConsumerWidget {
           );
         },
       ),
+      body: const JobsListView(),
     );
   }
 }
+
+class JobsListView extends ConsumerWidget {
+  const JobsListView ({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final firestoreRepository = ref.watch(firestoreRepositoryProvider);
+
+    return FirestoreListView<Job>(
+      query: firestoreRepository.jobsQuery(),
+      itemBuilder: (BuildContext context, QueryDocumentSnapshot<Job> doc) {
+        final job = doc.data();
+
+        return ListTile(
+          title: Text(job.title),
+          subtitle: Text(job.company),
+        );
+      },
+    );
+  }
+}
+
