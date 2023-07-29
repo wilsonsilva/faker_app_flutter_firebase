de22401ac124471434c2300af983544d8abd9e64
Wilson Silva <wilson.dsigns@gmail.com>
2023-07-29 11:16:08 +0700

    Configure Firebase for web, android and macos

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,16 @@
 import 'package:faker_app_flutter_firebase/src/routing/app_router.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const ProviderScope(child: MyApp()));
 }
 
